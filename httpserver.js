559962d8255d2021f3b42590bfac6b25873a1d30
RobinLuck <robinluck@yahoo.co.uk>
2015-09-29 18:49:57 +0100

    hjh

--- httpserver.js
+++ httpserver.js
@@ -2,8 +2,21 @@
 
 var http = require('http');
 
+var mappings = {
+	g: 'http://www.google.com'
+};
+
 var server  = http.createServer(function(req, res){
-	res.write('Hello world!');
+	var alias = req.url.substring(1);
+
+	if (!mappings[alias]){
+		res.writeHead(404);
+		return res.end();
+	}
+
+	res.writeHead(302, {
+		location: mappings[alias]
+	});
 	res.end();
 });
 
