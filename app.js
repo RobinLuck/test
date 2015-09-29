42f0b2f48189b094b8acae438966cd03e5bdc5a3
RobinLuck <robinluck@yahoo.co.uk>
2015-09-29 21:54:59 +0100

    include connect

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-for (var i = 0; i < 10; i++){
-console.log('hello '+i);
-}
+'use strict';
+
+var http = require('http'),
+	mappings = require('./mappings');
+
+var server  = http.createServer(function(req, res){
+	mappings.get(req.url, function(err, mapping){
+		if (err){
+		res.writeHead(404);
+		return res.end();
+		}
+
+		res.writeHead(302, {
+			location: mapping
+		});
+		res.end();
+	});
+});
+
+server.listen(3000);
